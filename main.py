d0af8710ee3d6b826e37b0ac017984525f45b7b6
Jared Mingle <102331218+Retrodox@users.noreply.github.com>
2024-04-25 13:20:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-from combine_factors import combine_factors
+from find_lcm import combine_factors
 from prime_factorization import prime_factors
 
-def main():
-    num1 = int(input("Enter the first number: "))
-    num2 = int(input("Enter the second number: "))
-    
-    factors1 = prime_factors(num1)
-    factors2 = prime_factors(num2)
-    
-    final_factors = combine_factors(factors1, factors2)
-    print(final_factors)
 
-    result = 1
-    for factor in final_factors:
-        result *= factor
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
     
-    print("The result of combining the prime factorizations is:", result)
+factors1 = prime_factors(num1)
+factors2 = prime_factors(num2)
 
-if __name__ == "__main__":
-    main()
+final_factors = combine_factors(factors1, factors2)
+print(final_factors)
+result = 1
+for factor in final_factors:
+    result *= factor
+
+print("The result of combining the prime factorizations is:", result)
