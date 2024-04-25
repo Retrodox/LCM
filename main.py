bb1d0d290e8771fffc6bdd592d90dfe71f1958b6
Jared Mingle <102331218+Retrodox@users.noreply.github.com>
2024-04-25 11:48:47 -0500

    Update

---
+++ main.py
@@ -0,0 +1,21 @@
+from combine_factors import combine_factors
+from prime_factorization import prime_factors
+
+def main():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    
+    factors1 = prime_factors(num1)
+    factors2 = prime_factors(num2)
+    
+    final_factors = combine_factors(factors1, factors2)
+    print(final_factors)
+
+    result = 1
+    for factor in final_factors:
+        result *= factor
+    
+    print("The result of combining the prime factorizations is:", result)
+
+if __name__ == "__main__":
+    main()
